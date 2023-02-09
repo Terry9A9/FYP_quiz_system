b63857ecd33d9577ac9e48f9589b4d4d3fb73042
Terry9A9 <terryleung087@gmail.com>
2023-02-09 22:20:35 +0800

    add select quiz room path

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,12 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import PlayQuiz from "./page/playQuiz"
+import EnterRoomId from "./page/selectQuizRoom"
 import 'bootstrap/dist/css/bootstrap.css';
 
 const router = createBrowserRouter([
   {
     path: "/play/quiz/:roomId",
     element: <PlayQuiz/>,
+  },
+  {
+    path: "/",
+    element: <EnterRoomId/>,
   },
 ]);
 
