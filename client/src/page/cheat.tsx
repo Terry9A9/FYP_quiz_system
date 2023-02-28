59d9f088987c0afcb15d89adee3c0ec6e495da9b
NG Ka Chun <s1276382@studentdmn.ouhk.edu.hk>
2023-02-28 16:04:21 +0800

    asdokjpo

--- client/src/page/cheat.tsx
+++ client/src/page/cheat.tsx
@@ -30,30 +30,71 @@ const cheat = () =>{
     const {classes} = useStyles();
     const [mouseMsg, setMouseMsg] = useState("" as string)
     const [OutCount,setOutCount] = useState(0 as number)
-    const [OutTime,setOutTime] = useState(0 as number)
-    const confirmBtn = document.getElementById("close_dialog");
-    const asd = setInterval(function() {setOutTime(OutTime+1)}, 1000)
-    return () => clearInterval(asd);
+    const [Time0,setTime0] = useState(0 as number)
+    const [Time1,setTime1] = useState(0 as number)
+    const [Time,setTime] = useState(0 as number)
+    const [enter,setEnter]= useState(false as boolean)
+    const [out,setOut]= useState(false as boolean)
+    const [timeone,setTimeone]=useState(false as boolean)
+
+
+    useEffect(()=>{
+        if(enter){
+            // let timeIn = Date.now();
+            let temp = Math.floor((Date.now()-Time0)/1000)
+            setTime((Time)=>(Time+temp))     
+            // let showTime = timeIn-Time0
+            console.log(temp+"+"+Date.now()+"-"+Time0)
+        }else{
+            // timeone?setTime0(Date.now()):setTimeone(true)
+            setTime0(Date.now)
+            setOutCount(OutCount+1)
+        }
+    },[enter])
+
+    // useEffect(()=>{
+    //     if(enter){
+    //         setTime1(Date.now())
+    //         // let timeIn = Date.now();
+    //         setTime((Time1-Time0))     
+    //         // let showTime = timeIn-Time0
+    //         console.log(Time+"+"+Time1+"-"+Time0)
+    //     }else{
+    //         timeone?setTime0(Date.now()):setTimeone(true)
+    //         setOutCount(OutCount+1)
+    //     }
+    // },[Time1])
+
+
     const handleMouseLeave = () => {
-    document.querySelector("#show");
-    let infoModal=document.querySelector("#infoModal");
+        // Time0=Date.now()
+        document.querySelector("#show");
         infoModal.showModal();
+        setEnter(false)
         setOutCount(OutCount+1)
-        setMouseMsg("mouse Leave!!")
-        console.log("mouse Leave!!")
+        // setMouseMsg("mouse Leave!!")
+        // console.log("mouse Leave!!")
+        // console.log("mouse Leave!!"+Time0)
     }
  
     const handleMouseEnter = () => {
         infoModal.close();
-        setMouseMsg("mouse Enter!!")
-        console.log("mouse Enter!!")
+        // timeone?setTime1(Date.now()):setTimeone(true)
+        setEnter(true)
+        // Time1=Date.now()
+        // setTime(Time+(timeIn-Time0))
+        // let showTime = timeIn-Time0
+        // setTime(Time1-Time0)
+        // setMouseMsg("mouse Enter!!")
+        // // console.log("mouse Enter!!"+showTime)
+        //  console.log(Time+"+"+Time1+"-"+Time0)
     }
 return (
     <div className={classes.root} onMouseLeave={() => handleMouseLeave()} onMouseEnter={() => handleMouseEnter()} tabIndex={0}
          style={{height: "100vh"}}>
         <meta name="viewport" content="initial-scale=1, width=device-width" />
         <Container>
-            {mouseMsg}{OutCount}{OutTime}
+            {mouseMsg}{OutCount}_______{Time}
             <br/>
             <dialog id="infoModal"style={{height: "100%",width:'100%'}} >
             <p>屌你</p>
