28d416926faa38c2903fb222a27c919fad819710
Terry9A9 <terryleung087@gmail.com>
2023-03-14 15:50:44 +0800

    animed ranking, waiting page

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, RouterProvider, Route, createRoutesFromElements } from "react-router-dom";
 import PlayQuiz from "./page/playQuiz"
 import EnterRoomId from "./page/selectQuizRoom"
+import { AnimatePresence } from "framer-motion"
 import 'bootstrap/dist/css/bootstrap.css';
 
 
@@ -19,6 +20,8 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>,
+    <AnimatePresence mode="wait">
+      <RouterProvider router={router} />
+    </AnimatePresence>
+  </React.StrictMode>
 )
--- client/src/page/playQuiz.tsx
+++ client/src/page/playQuiz.tsx
@@ -1,26 +1,64 @@
 import * as React from 'react';
 import {useState, useEffect} from 'react'
-import axios from 'axios'
 import webSocket from 'socket.io-client'
 import {useParams} from "react-router-dom";
 import {quiz, profile} from "../state";
-
+import StarsIcon from '@mui/icons-material/Stars';
+import MilitaryTechIcon from '@mui/icons-material/MilitaryTech';
+import SpeedIcon from '@mui/icons-material/Speed';
 import {Container, Row, Col} from 'react-bootstrap';
 import { makeStyles } from 'tss-react/mui';
-import { experimentalStyled as styled } from '@mui/material/styles';
-import {Button, Snackbar, Grid, Paper , FormControl, RadioGroup} from '@mui/material';
-import MuiAlert, { AlertProps } from '@mui/material/Alert';
+import {styled, useTheme} from '@mui/material/styles';
+import {
+    Button,
+    Grid,
+    Paper,
+    Box,
+    TextField,
+    Dialog,
+    useMediaQuery, DialogTitle, DialogContent, DialogActions, Badge,
+} from '@mui/material';
 
+import {useIsPresent, motion} from "framer-motion"
+
+import { Doughnut } from 'react-chartjs-2';
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
+
+ChartJS.register(ArcElement, Tooltip, Legend);
 const useStyles = makeStyles()((theme) => {
     return {
-        root: {
+        center:{
             display: 'flex',
             flexDirection: 'column',
             alignItems: 'center',
-        }
-        ,
+        },
+        root: {
+            height: "100vh",
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+        },
         textField: {
             margin: '20px 0'
+        },
+        waiting:{
+            height: "70%",
+            display: 'flex',
+            justifyContent: "center",
+            alignItems: "center"
+        },
+        sButton:{
+            display: 'flex',
+            width: "150px",
+            height: "40px",
+            borderRadius: "50px",
+            cursor: "pointer",
+            marginTop: "1vh",
+            justifyContent: "center",
+            alignItems: "center",
+        },
+        bg:{
+            background: "linear-gradient(90deg, rgba(238,174,202,1) 0%, rgba(90,226,201,1) 100%)"
         }
     }
 });
@@ -33,49 +71,44 @@ const Item = styled(Paper)(({ theme }) => ({
 }));
 
 
-const Alert = React.forwardRef<HTMLDivElement, AlertProps>(function Alert(
-    props,
-    ref,
-) {
-    return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
-});
 
 function PlayQuiz() {
+    const theme = useTheme();
+    const fullScreen = useMediaQuery(theme.breakpoints.down('md'));
 
     const [ws, setWs] = useState(webSocket('ws://localhost:3004'))
 
-    const [course, setCourse] = useState("")
-    const [quiz, setQuiz] = useState("")
+    const [myInfo, setMyInfo] = useState({})
+
+    const [course, setCourse] = useState("" as string)
+    const [quiz, setQuiz] = useState("" as string)
     const [questionSet, setQuestionSet] = useState([] as quiz["question_set"])
-    const [quizId, setQuizId] = useState("")
-    const [questionNum, setQuestionNum] = useState(-1)
-    const [selectedAnsIndex, setSelectedAnsIndex] = useState(-1)
-    const [totalPoint, setTotalPoint] = useState(0)
-    const [showRank, setShowRank] = useState(false)
+    const [quizId, setQuizId] = useState("" as string)
+    const [questionNum, setQuestionNum] = useState(-1 as number)
+    const [selectedAnsIndex, setSelectedAnsIndex] = useState(-1 as number)
+    const [totalPoint, setTotalPoint] = useState(0 as number)
+    const [showRank, setShowRank] = useState(false as boolean)
     const [rankInfo, setRankInfo] = useState([] as profile[])
     const [waitMsg, setWaitMsg] = useState(false as boolean)
-
-    const [IsStart, setIsStart] = useState(false as boolean)
-
+    const [nickName, setNickName] = useState("" as string)
+    const [isStart, setIsStart] = useState(false as boolean)
+    const [nickNameDialog, setNickNameDialog] = useState(true as boolean)
     //sync timer
     const [times, setTimes] = useState(-1 as number)
     const [timerStatus, setTimerStatus] = useState(false as boolean)
     //websocket room & Msg
     const [joinedRoom, setJoinedRoom] = useState(false as boolean)
     const [roomMsg, setRoomMsg] = useState("" as string)
+    const [roomInfo, setRoomInfo] = useState({})
     const [showRoomMsg, setShowRoomMsg] = useState(false as boolean)
     //anit cheating
     const [mouseMsg, setMouseMsg] = useState("" as string)
-
-
     const [OutCount,setOutCount] = useState(0 as number)
     const [Time0,setTime0] = useState(0 as number)
-    const [Time1,setTime1] = useState(0 as number)
     const [Time,setTime] = useState(0 as number)
     const [enter,setEnter]= useState(false as boolean)
-    const [out,setOut]= useState(false as boolean)
-    const [timeone,setTimeone]=useState(false as boolean)
 
+    const isPresent = useIsPresent();
 
     useEffect(()=>{
         if(enter){
@@ -112,10 +145,6 @@ function PlayQuiz() {
             return () => clearInterval(timer);
         } else if (times == 0){
             submit_ans()
-            //nextQuestion()
-            setWaitMsg(false)
-        }else {
-            setTimes(0)
             setWaitMsg(false)
         }
     }, [timerStatus, times])
@@ -141,7 +170,48 @@ function PlayQuiz() {
         )
     }
 
-    const QuestionComponent = () => {  //react component
+
+
+    function WaitingRoomComponent() {
+        return (
+            <>
+                <div
+                    className={classes.waiting}
+                >
+                    <motion.div
+                        style={{height:"70vh", width:"50vh"}}
+                        className={`${classes.bg}`}
+                        initial={{ height: "5vh", width: "5vh", borderRadius: 100 }}
+                        animate={{ height: "70vh", width: "100%", borderRadius: 30}}
+                        exit={{opacity:0}}
+                    >
+                        <Box
+                            sx={{
+                                p: 5,
+                                display: 'grid',
+                                gridTemplateColumns: { md: '1fr 1fr 1fr  1fr' },
+                                gap: 2,
+                            }}
+                        >
+                            {roomInfo.players && roomInfo?.players.map((e) =>
+                               ( <Item elevation={1} style={{textAlign: "center", width:"100%"}}>
+                                    {e.userName}
+                                </Item>)
+                            )}
+                            <Item elevation={1} style={{textAlign: "center"}}>
+                                <MilitaryTechIcon sx={{color:"#FFD700"}}/>{`law`}
+                            </Item>
+                            <Item elevation={1} style={{textAlign: "center"}}>
+                                <SpeedIcon/>{`timmy`}
+                            </Item>
+                        </Box>
+                    </motion.div>
+                </div>
+            </>
+        )
+    }
+
+    function QuestionComponent() {  //react component
         return (
             <>
                 <Row>
@@ -184,6 +254,93 @@ function PlayQuiz() {
         )
     }
 
+    let data = {
+        labels: [
+            'Red',
+            'Blue',
+            'Yellow'
+        ],
+        datasets: [{
+            label: 'My First Dataset',
+            data: [300, 50, 100],
+            backgroundColor: [
+                'rgb(255, 99, 132)',
+                'rgb(54, 162, 235)',
+                'rgb(255, 205, 86)'
+            ],
+            hoverOffset: 4
+        }]
+    }
+
+    function Rank(){
+        return (
+            <>
+                <Row className={classes.bg} style={{height: "80vh", borderRadius:"2vh"}}>
+                <Col style={{display: 'flex', alignItems:"center", justifyContent:"center"}}>
+                   <div> <Doughnut data={data} /></div>
+                </Col>
+                <Col>
+                    <Grid container alignItems="center" justifyContent="center" spacing={1} style={{height: "100%"}}>
+                            {rankInfo.map((e,index) => (
+                                <>
+                                    <Grid item lg={1}>
+                                        <Item >
+                                            {index+1}
+                                        </Item>
+                                    </Grid>
+                                    <Grid item lg={8}>
+                                        <Item >
+                                            {e.userName}
+                                        </Item>
+                                    </Grid>
+                                    <Grid item lg={3}>
+                                        <Item >
+                                            {e.totalPoint}
+                                        </Item>
+                                    </Grid>
+                                </>
+                            ))}
+                    </Grid>
+                </Col>
+                </Row>
+            </>
+        )
+    }
+
+    function StartButton() {
+        return (
+                <div className={classes.center}>
+                    <motion.div
+                        className={`${classes.sButton}`}
+                        whileHover={{ scale: 1.2 }}
+                        whileTap={{ scale: 0.9 }}
+                        transition={{ type: "spring", stiffness: 400, damping: 17 }}
+                        onClick={startQuiz}
+                        style={{backgroundColor: "#eeaeca"}}
+                    >
+                        Quiz Start
+                    </motion.div>
+                </div>
+            )
+    }
+
+    function NextButton() {
+        return (
+            <div className={classes.center} style={{float:"right"}}>
+                <motion.div
+                    className={classes.sButton}
+                    whileHover={{ scale: 1.2 }}
+                    whileTap={{ scale: 0.9 }}
+                    transition={{ type: "spring", stiffness: 400, damping: 17 }}
+                    onClick={nextQuestion}
+                    style={{backgroundColor: "#94bbe9"}}
+                >
+                    Next Question
+                </motion.div>
+            </div>
+        )
+    }
+
     const joinRoomMsg = () => {
         return (
             <h4>joined quiz room {roomId}</h4>
@@ -204,7 +361,12 @@ function PlayQuiz() {
             }
         });
         ws.on('join-room-message', (msg) => {
+            setMyInfo(msg);
             console.log(msg);
+        });
+        ws.on('room-info',(msg) => {
+            setRoomInfo(msg)
+            console.log(JSON.stringify(msg.players))
         });
         ws.on('room-brocast', (msg) => {
             setRoomMsg(msg)
@@ -245,6 +407,7 @@ function PlayQuiz() {
 
     const submit_ans = () => {
         if (!timerStatus) {
+
             let point = totalPoint
             console.log(selectedAnsIndex+"sel")
             if (selectedAnsIndex == questionSet[questionNum]?.correct) {
@@ -253,74 +416,66 @@ function PlayQuiz() {
                 setTotalPoint(point)
             }
             console.log(point)
-            ws.emit('point_submit', {roomId: roomId, totalPoint: point})
+            ws.emit('point-submit', {roomId: roomId, totalPoint: point})
         } else {
             setWaitMsg(true);
         }
     }
 
-    function Rank(){
-        return (
-            <>
-                <Grid container direction="column" alignItems="center" spacing={5}>
-                    <Grid item>
-                        {rankInfo.map((e,index) => (
-                            <>
-                                <Item>
-                                    {index+1}. {e.socketId} {e.totalPoint}
-                                </Item>
-                            </>
-                        ))}
-                    </Grid>
-                </Grid>
-            </>
-        )
-    }
-
     const handleMouseLeave = () => {
-        document.querySelector("#show");
-        IsStart ? infoModal.showModal():
         setEnter(false)
-        setOutCount(OutCount+1)
-        setMouseMsg("mouse Leave!!")
-        console.log("mouse Leave!!")
+        if (isStart){
+            document.querySelector("#show");
+            infoModal.showModal()
+            setOutCount(OutCount+1)
+            setMouseMsg("mouse Leave!!")
+            console.log("mouse Leave!!")
+        }
+
     }
 
     const handleMouseEnter = () => {
-        IsStart ? infoModal.close():
-        setEnter(true)
-        setMouseMsg("mouse Enter!!")
-        console.log("mouse Enter!!")
+        if (isStart){
+            infoModal.close()
+            setEnter(true)
+            setMouseMsg("mouse Enter!!")
+            console.log("mouse Enter!!")
+        }
+
     }
 
-
     return (
-        <div onMouseLeave={() => handleMouseLeave()} onMouseEnter={() => handleMouseEnter()} tabIndex={0}
-             style={{height: "100vh"}}>
+        <div
+        // onMouseLeave={() => handleMouseLeave()}
+        // onMouseEnter={() => handleMouseEnter()}
+        tabIndex={0}
+        style={{height: "100vh"}}
+        >
             <div className={classes.root}>
+                <br/>
+                <br/>
                 <Container>
                     <Row>
-                        {joinedRoom && joinRoomMsg()}
-                    </Row>
-                    <Row>
                         <Col>
-                            <Button variant="contained" color="primary" onClick={startQuiz}>
-                                Quiz Start
-                            </Button>
+                            {joinedRoom && joinRoomMsg()}
                         </Col>
                     </Row>
-                    <br/>
                     <Row>
                         <Col>
+                            <div style={{height:"12vh", backgroundColor:"red", marginBottom:"1vh"}}>Info</div>
+                        </Col>
+                    </Row>
+                    <Row>
+                        {waitMsg ? <WaitCountDown/> : showRank && isStart? <Rank/> : isStart? <QuestionComponent/>: <WaitingRoomComponent/>}
+                    </Row>
+                    <Row>
+                        <Col>
+                            {!isStart && <StartButton/>}
                             {questionNum + 1 < questionSet?.length ?
-                                <Button variant="contained" color="primary" onClick={nextQuestion}>next question</Button> : " "}
+                                <NextButton/> : " "}
                         </Col>
                     </Row>
-                    <Row>
-                        {waitMsg ? <WaitCountDown/> : showRank? <Rank/> : <QuestionComponent/>}
-                    </Row>
                     <br/>
-                    {mouseMsg}
                     <dialog id="infoModal"style={{height: "100%",width:'100%'}} >
                         <p>plz</p>
                     </dialog>
