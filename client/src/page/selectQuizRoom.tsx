573f47eebac36f8636381bf95a0e2a66e3ad09d8
Terry9A9 <terryleung087@gmail.com>
2023-03-31 22:07:26 +0800

    added missing import

--- client/src/page/selectQuizRoom.tsx
+++ client/src/page/selectQuizRoom.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { TextField, Button, Box , Grid} from '@mui/material';
 import { useNavigate  } from 'react-router-dom';
 import { makeStyles } from 'tss-react/mui';
